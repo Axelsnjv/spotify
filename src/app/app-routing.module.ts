3dde0c7b910a4281b5658b59f91b0c58b63f0e8f
Axelsnjv <asanjuanv1500@alumno.ipn.mx>
2024-04-24 16:41:28 -0600

    Error solucionado, no generaba el routing base -> --routing didnt work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component : ExampleComponent
+  }
+  //Agregando otra ruta
+/*
+  ,
+  {
+    path: "hello",
+    component : ExampleComponent
+  }
+*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
